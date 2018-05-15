e16434a39fc656a221746a57f9f089356bcaebef
Willy.Ting(丁穆仁) <willy.ting@vivotek.tw>
2018-05-15 14:34:38 +0800

    refactory with new instance and have mux.router. fix to green

--- machine/machine.go
+++ machine/machine.go
@@ -15,11 +15,22 @@ type Route struct {
 	HandlerFunc http.HandlerFunc
 }
 
-// RunServer start a http server on X port
-func RunServer(port int) {
-	router := mux.NewRouter().StrictSlash(true)
-	router.Methods("GET").Path("/").Name("hello").Handler(http.HandlerFunc(helloworld))
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), router)
+// Server ...
+type Server struct {
+	router *mux.Router
+}
+
+// NewServer ...
+func NewServer() *Server {
+	server := new(Server)
+	server.router = mux.NewRouter().StrictSlash(true)
+	return server
+}
+
+// Run start a http server on X port
+func (s *Server) Run(port int) {
+	s.router.Methods("GET").Path("/").Name("hello").Handler(http.HandlerFunc(helloworld))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), s.router)
 	if err != nil {
 		fmt.Print("init fail :", err)
 	}
@@ -30,6 +41,13 @@ func helloworld(w http.ResponseWriter, r *http.Request) {
 }
 
 // AddHandlers ...
-func AddHandlers(routes []Route) error {
+func (s *Server) AddHandlers(routes []Route) error {
+	for _, route := range routes {
+		s.router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
 	return nil
 }
--- machine/machine_test.go
+++ machine/machine_test.go
@@ -7,7 +7,8 @@ import (
 )
 
 func TestRunServer_hello(t *testing.T) {
-	go RunServer(80)
+	server := NewServer()
+	go server.Run(80)
 	resp, err := http.Get("http://localhost:80/")
 	if err != nil {
 		t.Error(err)
@@ -28,7 +29,8 @@ func TestRunServer_hello(t *testing.T) {
 	}
 }
 func TestRunServer_hello_8080_port(t *testing.T) {
-	go RunServer(8080)
+	server := NewServer()
+	go server.Run(8080)
 	resp, err := http.Get("http://localhost:8080/")
 	if err != nil {
 		t.Error(err)
@@ -48,3 +50,35 @@ func TestRunServer_hello_8080_port(t *testing.T) {
 		t.Error("response incorrect")
 	}
 }
+
+func TestRunServer_hello_bad_port(t *testing.T) {
+	server := NewServer()
+	go server.Run(-1)
+}
+
+/*func TestAddHandlers(t *testing.T) {
+	AddHandlers([]Route{
+		Route{"hello", "GET", "/hello", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, "hello world")
+		}},
+	})
+	go RunServer(80)
+	resp, err := http.Get("http://localhost:80/hello")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("get a error status")
+	}
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(contents) <= 0 {
+		t.Error("no response")
+	}
+	if string(contents) != "hello world" {
+		t.Error("response incorrect")
+	}
+}*/
