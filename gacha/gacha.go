15d43c62b6452fd0380b7527022c0986854d73d3
Willy.Ting(丁穆仁) <willy.ting@vivotek.tw>
2018-05-17 09:22:31 +0800

    finish first test case. get file API

--- gacha/gacha.go
+++ gacha/gacha.go
@@ -3,6 +3,8 @@ package gacha
 import (
 	"GaChaMachine/storage"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var cloud storage.Storage
@@ -14,5 +16,11 @@ func setupStorage(upload storage.Uploader, dlwonload storage.Downloader) {
 
 // GetRec ...
 func GetRec(w http.ResponseWriter, r *http.Request) {
-	cloud.Dlwonload.Download(&storage.FileInfo{}, w)
+	vars := mux.Vars(r)
+	cameraID := vars["cameraid"]
+	userID := r.Header.Get("X-identityID")
+	filePath := r.URL.Query().Get("p")
+	cloud.Dlwonload.Download(&storage.FileInfo{
+		FileName: userID + "/" + cameraID + "/" + filePath,
+	}, w)
 }
--- gacha/gacha_test.go
+++ gacha/gacha_test.go
@@ -5,37 +5,44 @@ import (
 	"GaChaMachine/storage"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 
 	"github.com/golang/mock/gomock"
 )
 
 func TestGetRec(t *testing.T) {
+	router := mux.NewRouter().StrictSlash(true)
+	router.Methods("GET").Path("/recstorage/{cameraid}").Name("test").HandlerFunc(GetRec)
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	mockUploader := mocks.NewMockDownloader(mockCtrl)
 	cloud.Dlwonload = mockUploader
-	mockUploader.EXPECT().Download(gomock.Any(), gomock.Any()).
+	mockUploader.EXPECT().Download(&storage.FileInfo{
+		FileName: "user/test/test.txt",
+	}, gomock.Any()).
 		Return(nil).Do(func(f *storage.FileInfo, w io.Writer) {
-		fmt.Fprint(w, "test123")
+		fmt.Fprint(w, "test")
 	})
-	req, err := http.NewRequest("GET", "/recstorage/test?p=test", nil)
+	req, err := http.NewRequest("GET", "http://localhost/recstorage/test?p=test.txt", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	req.Header.Add("X-identityID", "user")
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(GetRec)
-	handler.ServeHTTP(rr, req)
+	router.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
+		t.Errorf("handler returned wrong status code: got %d want %d",
 			status, http.StatusOK)
 	}
 	expected := "test"
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+	buf, err := ioutil.ReadAll(rr.Body)
+	if string(buf) != expected {
+		t.Errorf("handler returned unexpected body: got %s want %s",
+			string(buf), expected)
 	}
 }
